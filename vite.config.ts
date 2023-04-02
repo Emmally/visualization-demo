2ebad1c84d646bd1ba6d593367a3bb4cd8ba90bb
Emmally <xiaobing123#@>
2023-04-02 15:59:38 +0800

    feat: update package name

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// const path=require('path')
 import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({  
-  define:{global:{}},
+  // define:{global:{}},
   plugins: [vue()],
   resolve:{
     alias:{
