c1976f8090980d8e90787564ad731b6679739064
Emmally <xiaobing123#@>
2023-04-02 15:12:07 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div v-if="route.path==='/'" class="main-box">
+    <router-link v-for="item in menuList" :key="item.name" :to="{name: item.name}"> 
+      <div class="card">
+        <img :src="item.imgUrl" style="width:100%"/>
+        <div class="txt">
+          <p>{{item.des}}</p>
+        </div>
+      </div>
+    </router-link> 
+  </div>
+  <router-view></router-view>
+</template>
+
+
+<script setup lang="ts">
+import {useRoute} from 'vue-router'
+const route = useRoute();
+const menuList=[
+  {
+    name: 'earth',
+    imgUrl:'src/assets/images/earth/bg.jpg',
+    des:'echarts-gl实现地球可视化'
+  },
+  {
+    name: 'b',
+    imgUrl:'src/assets/images/bg.jpg',
+    des:'B页面'
+  }
+]
+</script>
+
+<style scoped>
+.main-box{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  justify-items: center;
+}
+.card{
+  width: 250px;
+  margin: 8px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+  text-align: center;
+}
+.txt{
+  padding: 10px;
+}
+</style>
---
+++ src/components/layout.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="title">{{title}}</div>
+    <div class="container" style="width: 100%;">
+        <slot/>   
+    </div>
+    <div class="button" @click="back"><span>返回主页</span></div>
+</template>
+<script lang="ts">
+    export default{
+        name:'Layout'
+    }
+</script>
+<script setup lang="ts">
+import {useRouter} from 'vue-router'
+const router = useRouter();
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  }
+});
+const back=()=>{
+    router.push({path:'/'})
+}
+</script>
+
+<style scoped>
+.title{
+   font-size: 30px;
+   font-weight: bold;
+   margin-bottom:10px;
+}
+.chart-container{
+    width: 100%;
+    height: 100%;
+}
+.button {
+    display: inline-block;
+    border-radius: 20px;
+    background-color: #f4511e;
+    border: none;
+    color: #ffff;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 400;
+    padding: 12px;
+    width: 150px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+    vertical-align: middle;
+}
+.button span {
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    transition: 0.5s;
+}
+.button span::after {
+    content: ">";
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    right: -20px;
+    transition: 0.5s;
+}
+.button:hover span {
+    padding-right: 30px;
+}
+.button:hover span::after {
+    opacity: 1;
+    right: 0;
+}
+</style>
