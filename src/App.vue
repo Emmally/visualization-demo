69120818048f0a6212210239309c8033c4ed155d
Emmally <xiaobing123#@>
2023-04-03 23:13:04 +0800

    feat: build

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,7 @@ const menuList=[
   margin: 8px;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
   text-align: center;
+  box-sizing: border-box;
 }
 .txt{
   padding: 10px;
