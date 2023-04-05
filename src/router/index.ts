6030df4fd07dd562489cf19c3502fa5c30834e77
Emmally <xiaobing123#@>
2023-04-05 18:40:55 +0800

    feat: 添加路由加载进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,28 @@
 import {createRouter,createWebHashHistory} from 'vue-router'
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 import routes from './routes'
 const router=createRouter({
     history:createWebHashHistory(),
     routes
 })
+
+NProgress.configure({
+    easing: 'ease',
+    speed: 500,
+    showSpinner: false,
+    trickleSpeed: 200,
+    minimum: 0.3
+})
+
+router.beforeEach((to, from, next)=>{
+    NProgress.start();
+    next();
+})
+
+router.afterEach(() => {
+    NProgress.done();
+})
+
 export default router
